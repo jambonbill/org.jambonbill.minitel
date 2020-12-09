b09f19e4b00eea341fef2e7cb34155b85c1b2f5a
orange <jambonbill@gmail.com>
2020-12-09 23:10:30 +0100

    private $_schema='minitel';

--- src/Minitel/Page.php
+++ src/Minitel/Page.php
@@ -17,6 +17,7 @@ class Page
 	private $_Base;
 	private $_user;
     private $_DEBUG=false;
+    private $_schema='minitel';
 
 	public function __construct (Base $Base)
     {
@@ -40,22 +41,85 @@ class Page
      */
     public function list()
     {
+        $sql="SELECT id, name FROM `minitel`.`page` WHERE id>0;";
+        $q=$this->db()->query($sql) or die(print_r($this->db()->errorInfo(), true) . "<hr />$sql");
 
+        $dat=[];
+        while($r=$q->fetch(PDO::FETCH_ASSOC)){
+            $dat[]=$r;
+        }
+        return $dat;
     }
 
-    public function create(string $name,strin $b64data)
+
+    /**
+     * Create a new videotex page record
+     * @param  string $name    [description]
+     * @param  string $b64data [description]
+     * @return [type]          [description]
+     */
+    public function create(string $name, string $b64data)
+    {
+        //convert b64 to datastr
+        $datastr='';
+        $strA=base64_decode($b64data);
+        for($j=0;$j<strlen($strA);$j++){//TO BINARY
+            $datastr.=$strA[$j];//chr
+        }
+
+        $sql ="INSERT INTO `minitel`.page (name, data, created_by, created_at) ";
+        $sql.="VALUES (".$this->db()->quote(trim($name)).", ".$this->db()->quote($datastr).",".$this->_uid().", NOW());";
+
+        $q=$this->db()->query($sql) or die(print_r($this->db()->errorInfo(), true) . "<hr />$sql");
+        $id=$this->db()->lastInsertId();
+
+        if (!$id) {
+            return false;
+        }
+
+        return +$id;
+    }
+
+    public function update(int $id)
     {
 
     }
 
-    public function update()
-    {
 
+
+    public function updatePermalink(int $id, string $permalink)
+    {
+        return $id;
     }
 
+
+    /**
+     * Delete one record
+     * @param  int    $id [description]
+     * @return [type]     [description]
+     */
     public function delete(int $id)
     {
+        $sql="DELETE FROM `minitel`.page WHERE id=$id AND created_by=".$this->_uid()." LIMIT 1;";
+        $this->db()->query($sql) or die(print_r($this->db()->errorInfo(), true) . "<hr />$sql");
+        //log?
+        return $id;
+    }
 
+    /**
+     * Import a videotex file (.vdt)
+     * @param  string $filename [description]
+     * @return [type]           [description]
+     */
+    public function import(string $filename)
+    {
+        if (!is_file($filename)) {
+            throw new Exception("$filename not found", 1);
+        }
+
+        $data=file_get_contents($filename);
+
+        return $this->create(basename($filename), base64_encode($data));
     }
 
 }
